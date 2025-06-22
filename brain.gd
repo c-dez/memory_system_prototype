071686e7e80f1bb3d6366dfc7968dbb070877dd8
c-dez <hdezcruzcarlos@gmail.com>
2025-06-22 17:56:26 -0600

    actualizado readme

--- brain.gd
+++ brain.gd
@@ -2,13 +2,8 @@ class_name Brain
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+# que necesito
+	# Progresión: La progresión del juego se dará con una variable invisible al jugador que medirá la relación del 1 al 100 denominada “nivel de crush” 
 
-	print("hello")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	#1 al 20: conocidos, 20 al 40: amigos, 40 al 60: friendzone, 60 al 80: crush, 80 al 100: Amantes
+	# Depende de las elecciones en el chat el jugador podrá sumar un valor de forma permanente, haciendo que si elige mal una cierta cantidad de veces no podrá desbloquear el final bueno con esa pareja, conforme aumente el medidor podrá ir desbloqueando 5 de 10 fotos únicas con cada una las candidatas
